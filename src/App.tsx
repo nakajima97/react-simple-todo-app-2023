3e2c22daca05d037dfe59710a043cc083197a152
Nakajima <kituneudon97@gmail.com>
2023-09-09 15:28:55 +0900

    静的なタスク一覧を作成 (#4)
    * #3 タスク一覧の静的なバージョンを作成
    
    * #1 stateの値を使ってタスク一覧を表示するようにした

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useState } from 'react';
 
-function App() {
-  const [count, setCount] = useState(0)
+function App(): JSX.Element {
+  const [tasks, setTasks] = useState(['タスク1', 'タスク2', 'タスク3']);
 
   return (
-    <div className="App">
+    <div>
+      <h1>Todoアプリ</h1>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <h2>タスク一覧</h2>
+        <div>
+          <ul>
+            {tasks.map((task, key) => (
+              <li key={key}>{task}</li>
+            ))}
+          </ul>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
